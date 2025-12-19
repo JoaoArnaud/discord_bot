474eb1894ab0660ca1caba8b491b6b977fc5be7c
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2025-12-19 18:23:28 -0300

    making /joke command with jokeAPI

--- src/bot.py
+++ src/bot.py
@@ -2,6 +2,7 @@ import hikari
 import lightbulb
 from dotenv import load_dotenv
 import os
+import aiohttp
 
 load_dotenv()
 
@@ -22,5 +23,27 @@ class Ping(
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
         await ctx.respond("Pong!")
+
+async def get_joke():
+    url = "https://v2.jokeapi.dev/joke/Any?lang=pt"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.json()
+
+            if data["type"] == "single":
+                return data["joke"]
+            else:
+                return f'{data["setup"]}\n\n{data["delivery"]}'
+@client.register()
+class Joke(
+    lightbulb.SlashCommand,
+    name="joke",
+    description="make a random joke in portuguese-br",
+):
+    @lightbulb.invoke
+    async def invoke(self, ctx: lightbulb.Context)-> None:
+        joke = await get_joke()
+        await ctx.respond(joke)
 
 bot.run()
